670025b414a658a2c95e11d0d6725da71dbe74e5
Paul Fenney <fenney@gmail.com>
2024-12-15 23:28:12 +0000

    Deploy all keymaps in our repo keymaps folder.
    Note that this doesn't delete any existing keymaps on the system!

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rsync -v keymap.xml kodi@localhost:/home/kodi/.var/app/tv.kodi.Kodi/data/userdata/keymaps/
+rsync -i keymaps/*.xml kodi@localhost:/home/kodi/.var/app/tv.kodi.Kodi/data/userdata/keymaps/
 
 ./kodi-send.sh reloadkeymaps

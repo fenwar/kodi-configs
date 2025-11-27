c327ec15fb4980293dd18ee4166cba07fd247685
Paul Fenney <fenney@gmail.com>
2025-11-27 17:51:34 +0000

    Skip steps of 15 and 30s.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 rsync -ri --delete --exclude="*.sw?" keymaps/ kodi@localhost:/home/kodi/.var/app/tv.kodi.Kodi/data/userdata/keymaps/
+rsync -ri userdata/advancedsettings.xml kodi@localhost:/home/kodi/.var/app/tv.kodi.Kodi/data/userdata/advancedsettings.xml
 
 ./kodi-send.sh reloadkeymaps
